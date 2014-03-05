e100d649e78be4eb34f05c2eb3a4da1a13fbb33b
jmcrosa <me@jmcrosa.com>
2014-03-04 18:27:31 -0600

    Added a way to log when the best attempts were made.

--- twitchplays.py
+++ twitchplays.py
@@ -18,6 +18,7 @@ prevstep = 3
 looking = 0
 step = 0
 furthest = 0
+path = []
 while 1:
     print("-------TRY:",tries,"-------- Furthest is ",furthest)
     print("currently in step ",step)
@@ -34,22 +35,32 @@ while 1:
             if step > 1 and nextstep == ((prevstep+2)%4):
                 print("Moved back ",directions[nextstep])
                 step = step-1
+                path.append(directions[nextstep])
                 if step < 0:
                     print("went off initial area")
                     step = 0
                     tries = tries + 1
+                    path = []
                 continue
             else:
                 print("Fell down in step ",step)
                 tries = tries +1
                 step = 0
+                path = []
                 continue
         else:
             print("moved ",directions[nextstep])
+            path.append(directions[nextstep])
             prevstep = nextstep
             step = step+1
             if step > furthest:
                 furthest=step
+                log = open("best_paths.txt","a")
+                log.write("Attempt "+str(tries)+": ")
+                for i in path:
+                    log.write(i+" ")
+                log.write("\n")
+                log.close()
             if step > len(steps):
                 print("Holy crap")
             continue
