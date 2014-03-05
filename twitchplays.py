5ed6ce471263ccb1ef2d11be1b82199eecd85f6a
jmcrosa <me@jmcrosa.com>
2014-03-04 19:39:33 -0600

    Weighted possibilities now coordinate with the path

--- twitchplays.py
+++ twitchplays.py
@@ -16,15 +16,50 @@ obstacles[5] = 1
 obstacles[10] = 3
 obstacles[15] = 1
 
-def getMove():
+#direction changes:
+#starts up
+#1-> right
+#3->up
+#6->left
+#9->up
+#12->right
+#15->up
+#17->left
+#18->up
+
+#Assume: 40% trolls. (push off path)
+#Asume: 40% actually do it in the right direction
+#Assume: 20% backtrack
+
+def getMove(target):
     r = random.randint(0,8)
-    if r < 3: #3/8
-        return 0 #up
-    if r < 5: #2/8
-        return 1 #right
-    if r < 77: #1/8
-        return 2 #down
-    return 3 # 2/8 = left
+    odds = [2,2,2,2]
+    if target < 1:          #favor up
+        odds = [3,2,1,2]
+    elif target < 4:        #favor right
+        odds = [2,3,1,2]
+    elif target < 7:        #favor up
+        odds = [3,2,1,2]
+    elif target < 10:       #favor left
+        odds = [2,1,3,2]
+    elif target < 13:       #favor up
+        odds = [3,2,1,2]
+    elif target < 16:       #favor right
+        odds = [2,3,1,2]
+    elif target < 18:       #favor up
+        odds = [3,2,1,2]
+    elif target < 19:       #favor left
+        odds = [2,1,3,2]
+    else:                   #favor up
+        odds = [3,2,1,2]
+
+    if r < odds[0]:
+        return 0
+    if r < odds[0]+odds[1]:
+        return 1
+    if r < odds[0]+odds[1]+odds[2]:
+        return 2
+    return 3
 
 
 tries = 1
@@ -34,7 +69,7 @@ step = 0
 furthest = 0
 path = []
 while 1:
-    nextstep = getMove()
+    nextstep = getMove(step)
     if (tries %100) == 0:
         print("-------TRY:",tries,"-------- Furthest is ",furthest)
 #    print("currently in step ",step)
